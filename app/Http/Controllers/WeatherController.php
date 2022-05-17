753d98075a9ff2a98ad9752453c0c0733ec2ce52
Hovhannes111 <hovhanness28@gmail.com>
2022-05-17 14:56:42 +0400

    cache fix

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -28,20 +28,18 @@ class WeatherController extends Controller
     */
     public function getWeather(Request $request): JsonResponse
     {
-        if( Cache::get('weather') && 
-            Cache::get('weather')->latitude === $request->latitude &&
-            Cache::get('weather')->longitude === $request->longitude )
-        {
-            $data = Cache::get('weather');
+        if(Cache::get('weather_id')) {
+            $weather_id = Cache::get('weather_id');
+            $data = Weather::findOrFail($weather_id);
             $result = $data->temp;
         } else {
-            Cache::pull('weather');
-            $expires_at = Carbon::now()->addHour(1)->format('H:i:s'); 
+            Cache::pull('weather_id');
+            $expires_at = Carbon::now()->addHour(1)->format('Y-m-d H:i:s'); 
             $data = new stdClass;
             $data->latitude = $request->latitude;
             $data->longitude = $request->longitude;
             $result = $this->getWeather->getResult($data);
-            $temp = Weather::firstOrNew([
+            $temp = Weather::firstOrCreate([
                 'country_id' => $request->country_id,
                 'state_id'   => $request->state_id ?? null,
                 'city_id'    => $request->city_id ?? null,
@@ -51,7 +49,7 @@ class WeatherController extends Controller
                 'expires_at' => $expires_at
             ]);
             $temp->save;
-            Cache::add('weather', $temp, $this->seconds);
+            Cache::add('weather_id', $temp->id, $this->seconds);
         }
         return response()->json($result);
     }
